a2e4b688805d3f771216a47d4ba7af8624bb342d
Chriztian Steinmeier <chriztian@steinmeier.dk>
2022-03-31 00:57:15 +0200

    Build v0.5

---
+++ build/grid-overlay.js
@@ -0,0 +1,16 @@
+(function() {
+	function attachGrid() {
+		let gridElement = document.createElement('div')
+		gridElement.setAttribute('id', 'grid-overlay')
+
+		let columnCount = window.getComputedStyle(document.documentElement).getPropertyValue('--go-columns')
+
+		for (let c=0; c<columnCount; c++) {
+			gridElement.appendChild(document.createElement('div'))
+		}
+
+		document.querySelector('body').appendChild(gridElement)
+	}
+
+	document.addEventListener('DOMContentLoaded', () => { attachGrid()	})
+})()
