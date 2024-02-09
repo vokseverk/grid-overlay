0bf604849bfe223b76b36154ed7b327ea65edb7f
Chriztian Steinmeier <chriztian@steinmeier.dk>
2024-02-09 06:39:52 +0100

    Rewrite JS as class

--- src/grid-overlay.js
+++ src/grid-overlay.js
@@ -1,16 +1,20 @@
-(function() {
-	function attachGrid() {
+class GridOverlay {
+	constructor() {
+		this.attachGrid()
+	}
+
+	attachGrid() {
 		let gridElement = document.createElement('div')
 		gridElement.setAttribute('id', 'grid-overlay')
 
 		let columnCount = window.getComputedStyle(document.documentElement).getPropertyValue('--go-columns')
 
-		for (let c=0; c<columnCount; c++) {
+		for (let c = 0; c < columnCount; c++) {
 			gridElement.appendChild(document.createElement('div'))
 		}
 
 		document.querySelector('body').appendChild(gridElement)
 	}
+}
 
-	document.addEventListener('DOMContentLoaded', () => { attachGrid()	})
-})()
+new GridOverlay()
